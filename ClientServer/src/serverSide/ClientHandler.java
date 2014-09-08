461550e4389238767c4c39f481fc5c36ebd0f3d8
Thomas Hedegaard Jensen <ThomasHedegaard@Thomas-Hedegaard-Jensens-MacBook-Pro.local>
2014-09-08 20:59:14 +0200

    All set for server package

---
+++ ClientServer/src/serverSide/ClientHandler.java
@@ -0,0 +1,14 @@
+package serverSide;
+
+public class ClientHandler implements Runnable {
+
+    public ClientHandler(){
+    }
+    
+    // Thread method
+    @Override
+    public void run() {
+
+    }
+
+}
--- ClientServer/src/serverSide/ServerStartup.java
+++ ClientServer/src/serverSide/ServerStartup.java
@@ -1,15 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package serverSide;
 
-/**
- *
- * @author ThomasHedegaard
- */
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerStartup {
+
+    
+    
+    
+    public static void main(String[] args) {
+        try{
+        ServerSocket listen = new ServerSocket(7070);
+        
+            while (true) {
+                Socket client = listen.accept();
+                
+                
+            }
+        
+        }catch(IOException e){
+        }
+        
+        
+        
+    }
     
 }
