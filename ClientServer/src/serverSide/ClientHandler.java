06fcdefe0c9a9deae8ed4c6d9dad4b27f82f365f
thomashed <thomashj93@gmail.com>
2014-09-08 21:43:03 +0200

    Added functionality in serverSide classes

--- ClientServer/src/serverSide/ClientHandler.java
+++ ClientServer/src/serverSide/ClientHandler.java
@@ -1,10 +1,31 @@
 package serverSide;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class ClientHandler implements Runnable {
 
-    public ClientHandler(){
+    private Socket clientSocket;
+    private ArrayBlockingQueue<String> msg;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    // Constructor
+    public ClientHandler(Socket con, ArrayBlockingQueue msg) throws IOException {
+        this.clientSocket = con;
+        this.msg = msg;
+        openStreams();
     }
-    
+
+    private void openStreams() throws IOException {
+        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        this.out = new PrintWriter(clientSocket.getOutputStream());
+    }
+
     // Thread method
     @Override
     public void run() {
--- ClientServer/src/serverSide/ServerStartup.java
+++ ClientServer/src/serverSide/ServerStartup.java
@@ -3,27 +3,28 @@ package serverSide;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class ServerStartup {
 
-    
-    
-    
+    private static ArrayBlockingQueue<String> msg;
+
+    // Main
     public static void main(String[] args) {
-        try{
-        ServerSocket listen = new ServerSocket(7070);
-        
+        try {
+            ServerSocket listen = new ServerSocket(7070);
+            msg = new ArrayBlockingQueue(20);
+
+            
+            
             while (true) {
                 Socket client = listen.accept();
-                
-                
+                new Thread(new ClientHandler(client, msg)).start();
             }
-        
-        }catch(IOException e){
+
+        } catch (IOException e) {
         }
-        
-        
-        
+
     }
-    
+
 }
